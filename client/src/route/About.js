3b7fb4e757188841deedf922424769dc6d9c2c37
Ubuntu <ubuntu@ip-172-31-61-99.us-west-2.compute.internal>
2020-09-09 15:32:00 +0000

    Client About copy function added

--- client/src/route/About.js
+++ client/src/route/About.js
@@ -1,14 +1,36 @@
 import React, {useState, useEffect} from "react"
 import {domain, googleMapAPI} from "../config.js"
-impot {RiFileCopyLine} from "react-icons/ri"
+import {RiFileCopyLine} from "react-icons/ri"
 import "../css/About.css"
 
 const About = props => {
     const [location, setLocation] = useState("106 Legion Way SE")
     const [location2, setLocation2] = useState("Olympia, WA 98501")
     const [phone, setPhone] = useState("(360)-705-8000")
+    const [copySucceed, setCopySucceed] = useState(false)
+    const [copySucceed2, setCopySucceed2] = useState(false)
     const [schedule, setSchedule] = useState([])
     const [googleAPI, setGoogleAPI] = useState(`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJVW-dpaB1kVQR7GkEIBBF6Bc&key=${googleMapAPI}`)    
+    function copyLocation(){
+	var textField = document.createElement('textarea')
+        textField.innerText = '106 Legion Way SE, Olympia, WA 98501'
+        document.body.appendChild(textField)
+        textField.select()
+        document.execCommand('copy')
+        textField.remove()
+	setCopySucceed(true);
+	setTimeout(()=>{setCopySucceed(false)},2000)
+    }
+    function copyPhone(){
+	var textField = document.createElement('textarea')
+    	textField.innerText = '3607058000'
+    	document.body.appendChild(textField)
+    	textField.select()
+    	document.execCommand('copy')
+    	textField.remove()
+	setCopySucceed2(true);
+        setTimeout(()=>{setCopySucceed2(false)},2000)
+    }
 
     useEffect(() => {
         fetch(`${domain}client/hours/get`, {
@@ -41,11 +63,11 @@ const About = props => {
                     <div className="aboutTitle">Location</div>
                     <div>
                         <div>{location}</div>
-                        <div>{location2}<RiFileCopyLine size='25'/></div>
+                        <div>{location2}<RiFileCopyLine size='20'onClick={copyLocation}/>{copySucceed?<span style={{color:'gray'}}>copied</span>:null}</div>
                     </div>
 		    <div className="aboutTitle">Phone</div>
                     <div>
-                        <div>{phone}<RiFileCopyLine size='25' /></div>
+                        <div>{phone}<RiFileCopyLine size='20' onClick={copyPhone}/>{copySucceed2?<span style={{color:'gray'}}>copied</span>:null}</div>
                     </div>
                     <div>
                         <div className="aboutTitle">Hours</div>
