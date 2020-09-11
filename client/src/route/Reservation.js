57c5837af78a363d58e20875d49311a1b469b442
Ubuntu <ubuntu@ip-172-31-61-99.us-west-2.compute.internal>
2020-09-11 20:58:44 +0000

    Client reservation page calander added

--- client/src/route/Reservation.js
+++ client/src/route/Reservation.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 const Reservation = (props)=>{
+    const [selectedDay, setSelectedDay] = useState(null)
+    function handleDayClick(day){
+      setSelectedDay(day)
+    }
     return(
-        <div className='bodyContainer'></div>
+        <div className='bodyContainer'>
+	  <DayPicker
+	    selectedDays={selectedDay}
+	    onDayClick={handleDayClick}
+	    fromMonth={new Date()}
+            toMonth={new Date(Date.now()+(3600*1000*24*60))} //60days
+	    disabledDays={[
+		    {before: new Date(Date.now() + ( 3600 * 1000 * 24))},
+	    ]}  
+	  />    
+	</div>
     )
 }
 
