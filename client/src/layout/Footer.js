73603644b5ee384a327a47ec87dcec36f46ca737
Yoon Jung <dannydannyl@me.com>
2020-09-20 18:42:06 -0400

    A tag added for facebook, insta, yelp links

--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
-import {AiFillFacebook, AiFillInstagram, AiFillTwitterCircle} from 'react-icons/ai';
+import {AiFillFacebook, AiFillInstagram} from 'react-icons/ai';
+import {ImYelp} from 'react-icons/im'
 import '../css/Footer.css';
 
 const Footer = (props)=>{
     let d = new Date();
     let n = d.getFullYear();
     let [currentYear, setCurrentYear] = useState(n)
+    
     return(
         <div id='footerContainer'>
             <div id='footerCopyRight'>Copyright © {currentYear} Asahi Sushi All rights reserved.</div>
             <div id='footerSocialContainer'>
-                <div><AiFillFacebook color="gray" size="25px"/></div>
-                <div><AiFillInstagram color="gray" size="25px"/></div>
-                <div><AiFillTwitterCircle color="gray" size="25px"/></div>
+                <a href="https://facebook.com/Asahi-Sushi-102796514642869"><AiFillFacebook color="gray" size="25px"/></a>
+                <a href="https://www.instagram.com/asahi_sushi_olympia/?hl=en"><AiFillInstagram color="gray" size="25px"/></a>
+                <a href="https://www.yelp.com/biz/asahi-sushi-olympia?osq=asahi+sushi+olympia"><ImYelp color="gray" size="25px"/></a>
             </div>
             
         </div>
