0ddaa8fbb76b0c981db4ed6a31f1a3e57cc5127b
Yoon Jung <dannydannyl@me.com>
2020-09-21 18:14:44 -0400

    Client confirmation page edited

--- client/src/route/Confirmation.js
+++ client/src/route/Confirmation.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Confirmation = (props)=>{
     return(
         <div className='bodyContainer'>
-            <div>Confirmation</div>
-            <div>Thank you for your order.</div>
+            <div>Thank you for your order!</div>
+            <div>We have recieved your order. Your order will be ready within 20 minutes.</div>
         </div>
     )
 }
