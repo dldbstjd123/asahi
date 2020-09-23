229045e90bf0b279b8f64c077951936d8accf9fd
Yoon Jung <dannydannyl@me.com>
2020-09-22 22:18:42 -0400

    Client confirmation shows order id

--- api/routes/clover.js
+++ api/routes/clover.js
@@ -50,7 +50,7 @@ router.post("/proceed", async function (req, res, next) {
     let charge = await chargeOrder(access_token, source, orderId)
     console.log(`final reuslt charge = ${JSON.stringify(charge)}`)
     if (charge.id) {
-        res.json({ status: 1, items: charge.items, orderId: charge.id })
+        res.json({ status: 1, items: charge.items, id: charge.id })
         sendEmail(req.body.email)
         return
     } else {
