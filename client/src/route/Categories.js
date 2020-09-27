4eda61f2cb5f6e1729f5442775fcafaa5e1b9801
Yoon Jung <dannydannyl@me.com>
2020-09-27 12:26:31 -0400

    client categories page added

--- client/src/route/Categories.js
+++ client/src/route/Categories.js
@@ -1,7 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { domain } from "../config.js"
 import "../css/Categories.css"
 
 const Categories = props => {
+    const [list, setList] = useState()
+    const [listLength, setListLength] = useState(0)
+
+    function mouseOver() {
+        //console.log("mouseOver")
+    }
+    function mouseEnter(event) {
+        console.log("mouseEnter")
+        console.log(event.target)
+    }
+    function mouseLeave(event) {
+        console.log("mouseLeave")
+        console.log(event.target)
+    }
+    useEffect(() => {
+        fetch(`${domain}client/category/get`, {
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setListLength(data.length)
+                setList(data)
+            })
+    }, [])
     return (
         <div className="bodyContainer">
             <div id="categoriesRouteContainer">
@@ -21,6 +52,27 @@ const Categories = props => {
                     <img src="/images/home/home4.jpg" />
                     <div className="categoriesBoxContainer"></div>
                 </div>
+                {list === undefined ? (
+                    <></>
+                ) : (
+                    list.map((item, i) => {
+                        let url = "/images/category/" + item.image
+                        return (
+                            <div
+                                className="categoriesImageContainer"
+                                key={i}
+                                onMouseOver={mouseOver}
+                                onMouseEnter={mouseEnter}
+                                onMouseLeave={mouseLeave}
+                            >
+                                <img src={url} />
+                                <div className="categoriesBoxContainer">
+                                    {item.name}
+                                </div>
+                            </div>
+                        )
+                    })
+                )}
             </div>
         </div>
     )
