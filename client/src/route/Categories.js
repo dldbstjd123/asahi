a4ebb7c829db0c7d0962906971c9ae681b8e87d9
Yoon Jung <dannydannyl@me.com>
2020-09-27 12:48:36 -0400

    Client categories mouse over effect added

--- client/src/route/Categories.js
+++ client/src/route/Categories.js
@@ -35,22 +35,6 @@ const Categories = props => {
     return (
         <div className="bodyContainer">
             <div id="categoriesRouteContainer">
-                <div className="categoriesImageContainer">
-                    <img src="/images/home/home1.jpg" />
-                    <div className="categoriesBoxContainer">Specialty Roll</div>
-                </div>
-                <div className="categoriesImageContainer">
-                    <img src="/images/home/home2.jpg" />
-                    <div className="categoriesBoxContainer">TEST</div>
-                </div>
-                <div className="categoriesImageContainer">
-                    <img src="/images/home/home3.jpg" />
-                    <div className="categoriesBoxContainer"></div>
-                </div>
-                <div className="categoriesImageContainer">
-                    <img src="/images/home/home4.jpg" />
-                    <div className="categoriesBoxContainer"></div>
-                </div>
                 {list === undefined ? (
                     <></>
                 ) : (
