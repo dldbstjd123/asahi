5824be51b4465eff7d821e808117280e93113aea
Yoon Jung <dannydannyl@me.com>
2020-10-03 22:55:52 -0400

    Client About page right side position update minor change

--- client/src/route/About.js
+++ client/src/route/About.js
@@ -87,7 +87,7 @@ const About = props => {
                                 {location}
                             </a>
                         </div>
-                        <div>
+                        <div id="aboutRightSidePhone">
                             <a
                                 href="https://www.google.com/maps/place/Asahi+Sushi/@47.04334,-122.9010329,15z/data=!4m5!3m4!1s0x0:0x17e84510200469ec!8m2!3d47.04334!4d-122.9010329"
                                 style={{
